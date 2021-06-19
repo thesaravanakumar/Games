ca0512f71a365ca574531b001da2a138428808cd
Saravana Kumar <saravana.k.manimaran@gmail.com>
2021-06-19 13:45:03 +0530

    BasketBall Game

---
+++ Basket Ball/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject trial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trial.SetActive(false);
+    }
+
+    public void Activetrial()
+    {
+        trial.SetActive(true);
+    }
+}
---
+++ Basket Ball/Scripts/ScoreArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+    public GameObject effectobject;
+
+    private void Start()
+    {
+        effectobject.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Ball>() != null)
+        {
+            effectobject.SetActive(true);
+        }
+    }
+}
